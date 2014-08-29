bdf1d99deb9f1de952746af729fbfb2dc78bd02d
JJ Idt <johnidt@gmail.com>
2014-08-29 12:14:51 -0700

    add user table

--- db/migrate/20140829191005_create_votes.rb
+++ db/migrate/20140829191005_create_votes.rb
@@ -1,6 +1,9 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    create_table :votes do |t|
+    create_table :user do |t|
+      t.string :name
+      
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829190857) do
+ActiveRecord::Schema.define(version: 20140829191005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20140829190857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "user", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
