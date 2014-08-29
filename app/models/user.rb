4016e845455b7c66fe449eac9ce08d4f47d96c49
JJ Idt <johnidt@gmail.com>
2014-08-29 15:06:58 -0700

    add user password confirmation accessors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :password
+  attr_accessor :password_confirmation
+
   validates_presence_of :name
   validates_presence_of :password_digest
-  
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe User do
+
+  before do
+    @user = User.new(name: "Testuser", password: "foobar", password_confirmation: "foobar")
+  end
+
   it { should respond_to :name }
   it { should respond_to :password_digest }
   it { should validate_presence_of :name }
   it { should validate_presence_of :password_digest }
+  
 end
