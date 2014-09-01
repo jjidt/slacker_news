a8e190fb66c48d017577a97ca7d46eb692c32416
JJ Idt <johnidt@gmail.com>
2014-08-31 21:43:20 -0700

    add further validation for user votes

--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -13,6 +13,17 @@ describe Vote do
   end
 
   it "should let a user vote for multiple links" do
+    test_vote = Vote.new({:user_id => 1, :link_id => 2})
+    test_vote.save
+    test_vote2 = Vote.new({:user_id => 1, :link_id => 3})
+    test_vote2.save
+    expect(test_vote2.save).to eq true
+  end
 
+  it "should let multiple users vote for a link" do
+    test_vote = Vote.new({:user_id => 1, :link_id => 2})
+    test_vote.save
+    test_vote2 = Vote.new({:user_id => 2, :link_id => 2})
+    expect(test_vote2.save).to eq true
   end
 end
